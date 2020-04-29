aa055e8e0b375e928660ff42fd704652926e5ab8
Muthomi Mate <muthomimate@gmail.com>
2020-04-29 16:42:46 +0300

    add styled components library

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,39 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
 
+const StyledHeader = styled.div`
+  background: rebeccapurple;
+  marginBottom: 1.45rem;
+
+  div {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 1.45rem 1.0875rem;
+  }
+  h1 {
+    margin: 0;
+  }
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+  <StyledHeader
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
+    <div>
+      <h1>
+        <StyledLink
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
         >
           {siteTitle}
-        </Link>
+        </StyledLink>
       </h1>
     </div>
-  </header>
+  </StyledHeader>
 )
 
 Header.propTypes = {
